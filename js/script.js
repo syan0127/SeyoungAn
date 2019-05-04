9c951e3356f85115cdf93474a4547dbb78ed4409
syan0127 <syan0127@gmail.com>
2019-05-04 15:17:43 -0400

    small change

--- js/script.js
+++ js/script.js
@@ -12,42 +12,6 @@ for (i = 0; i < dropdown.length; i++) {
 	});
 }
 
-// $(document).ready(function(){
-//     $(".in").click(function () {
-//       $("a.active").removeClass('active');
-//       $(this).addClass('active');
-//       var active_section = $(this).attr('href'); //get active section id
-//       $('html, body').animate({
-//       //and scroll to the section
-//       scrollTop: $(active_section).offset().top
-//       }, 1000);
-//     });
-  
-//    $(window).scroll(function (event) {
-//         var scroll = $(window).scrollTop();
-//         if(scroll <= 1100){
-//          $("a").removeClass('active');
-//          $("#nav-item-1").addClass('active');
-//       }
-//       else if(scroll > 1100 && scroll <= 2200){
-//          $("a").removeClass('active');
-//          $("#nav-item-2").addClass('active');
-//       }
-//       else if(scroll > 2200 && scroll <= 3110){
-//          $("a").removeClass('active');
-//          $("#nav-item-3").addClass('active');
-//       }
-//       else if(scroll > 3110 && scroll <= 4020){
-//          $("a").removeClass('active');
-//          $("#nav-item-4").addClass('active');
-//       }
-//       else if(scroll > 4020){
-//          $("a").removeClass('active');
-//          $("#nav-item-5").addClass('active');
-//       }
-//     });
-  
-//  });
 
 function myFunction() {
   var x = document.getElementById("myTopnav");
